dda29556beac61a356094be0769a185936ad0336
allerter <a.hazhir@gmail.com>
2024-01-07 13:38:57 +0100

    Update PrimeFollower constructor and attributes

--- src/dps/platoon/PrimeFollower.java
+++ src/dps/platoon/PrimeFollower.java
@@ -3,22 +3,15 @@ package dps.platoon;
 import java.io.IOException;
 
 import dps.GPSLocation;
-import dps.truck.SocketAddress;
 import dps.truck.Truck;
+import dps.truck.TruckServer;
 
 public class PrimeFollower extends Truck implements PlatoonTruck {
 
     private Platoon platoon; 
 
-    public PrimeFollower(int id, String direction, float speed, GPSLocation destination, GPSLocation location, SocketAddress socketAddress, Platoon platoon) throws IOException {
-        super(id, direction, speed, destination, location, socketAddress);
-        this.platoon = platoon;
-    }
-
-    public Platoon getPlatoon() {
-        return platoon;
-    }
-    public void setPlatoon(Platoon platoon) {
+    public PrimeFollower(int id, String direction, double speed, GPSLocation destination, GPSLocation location, TruckServer server, Platoon platoon) throws IOException {
+        super(id, direction, speed, destination, location, server);
         this.platoon = platoon;
     }
 
