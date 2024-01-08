c740e9c652bb50f5df2feec64a7dedcd10321299
Mohammed Rizwan <mohammedrizwan0909@gmail.com>
2024-01-08 21:21:44 +0100

    Revert "Mohammed rizwan"

--- src/dps/Utils.java
+++
@@ -1,9 +0,0 @@
-package dps;
-
-import java.time.LocalDate;
-
-public class Utils {
-    public static String now() {
-        return LocalDate.now().toString();
-    }
-}
--- src/dps/platoon/PlatoonTruck.java
+++
@@ -1,6 +0,0 @@
-package dps.platoon;
-
-public interface PlatoonTruck {
-    public void disconnect();
-    public void discover();
-}
