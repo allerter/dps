1380303fd8921a7cb4e7ffbc33c5f8e80909c849
allerter <a.hazhir@gmail.com>
2024-01-18 22:33:03 +0100

    Add GridMap

--- src/map/GridMap.java
+++ src/map/GridMap.java
@@ -26,7 +26,6 @@ public class GridMap {
         }
 
     }
-
     // Function to print the grid map
     public void printGrid() {
         for (int i = 0; i < grid.length; i++) {
@@ -73,5 +72,56 @@ public class GridMap {
 
     public static int calculateDistance(Location location, Location location2) {
         return (location.getRow() - location2.getRow()) + (location.getColumn() - location2.getColumn());
+    }
+
+    synchronized public void update(TruckInfo[] truckInfos) {
+        for (TruckInfo truckInfo : truckInfos) {
+            int id = truckInfo.getId();
+            int speed = truckInfo.getSpeed();
+            Direction direction = truckInfo.getDirection();
+
+            // Move each truck to new position based on speed and direction
+            for (int i = 0; i < truckInfos.length; i++) {
+                int[] truckHead = findElement("Head" + id);
+                // If direction is north, trucks go straight up in array
+                // Otherwise, they will move 1 column to left or right
+                int destCol;
+                if (direction == Direction.NORTH){
+                    destCol = truckHead[1];
+                } else if (direction == Direction.NORTH_EAST){
+                    destCol = truckHead[1] + 1;
+                } else if (direction == Direction.NORTH_WEST){
+                    destCol = truckHead[1] - 1;
+                } else {
+                    throw new IllegalArgumentException("Truck has illegal direction: " + direction);
+                }
+                moveTruck(truckHead[0], truckHead[1], truckHead[0] - speed, destCol);
+            }
+
+        }
+    }
+
+    // Method to find the position of an element in a 2D array
+    private int[] findElement(String target) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j].equals(target)) {
+                    return new int[]{i, j}; // Return the position if the element is found
+                }
+            }
+        }
+        return null; // Return null if the element is not found
+    }
+
+    private void moveTruck(int sourceRow, int sourceCol, int destRow, int destCol) {
+        // Move head
+        grid[destRow][destCol] = grid[sourceRow][sourceCol];
+        grid[sourceRow][sourceCol] = "-";
+
+        // Move tail
+        for (int i = 1; i <= 2; i++) {
+            grid[destRow + i][destCol] = grid[sourceRow + i][sourceCol];
+            grid[sourceRow + i][sourceCol] = "-";
+        }
     }
 }
